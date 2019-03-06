f11f48a3029e07c6dfee58fb4177e6ea8ec51dfa
liuhuihui <244701374@qq.com>
2019-03-06 10:34:35 +0800

    测试基础组件

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-antd-com
 测试react组件发布到npm
+
+190306 : 测试
---
+++ src/components/hello.js
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+import './hello.css';
+
+/**
+ * @author hui
+ * @date 2019/3/5
+ * @Description: 测试
+*/
+
+class Hello extends Component{
+  constructor(props) {
+      super(props);
+      this.state = {}
+  }
+
+  render(){
+    return (
+        <div>Hello World !</div>
+    )
+  }
+}
+
+export default Hello;
